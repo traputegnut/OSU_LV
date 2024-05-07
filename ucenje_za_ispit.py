81ab6ac789daad9755211c6d63e71267da5c6ade
traputegnut <156333358+traputegnut@users.noreply.github.com>
2024-05-07 09:43:28 +0200

    Add files via upload

---
+++ ucenje_za_ispit.py
@@ -0,0 +1,204 @@
+#LV1--------------------------------------------
+#1.4.1.
+print("unesite broj radnih sati")
+hour=int(input())
+print("unesite satnicu")
+perHour=float(input())
+print("iznos:",+ float(hour*perHour))
+
+def CalculateWage(hour,perHour):
+    return hour*perHour
+
+print(CalculateWage(hour,perHour),'eura')
+
+#1.4.2.
+try:
+    print("unesi broj:")
+    broj=float(input())
+except:
+    print("nije unesen broj")
+if broj<0.0 or broj>1.0:
+    print("broj izvan intervala")
+elif broj<0.6:
+    print("F")
+elif broj>0.6 and broj<0.7:
+    print("D")
+elif broj>0.7 and broj<0.8:
+    print("C")
+elif broj>0.8 and broj<0.9:
+    print("B")
+else:
+    print("A")
+
+#1.4.3.
+def averageValue(brojevi):
+    sum=0
+    for broj in brojevi:
+        sum+=sum
+    return sum/len(brojevi)
+
+brojevi=[]
+while True:
+    unos=input()
+    if(unos="Done"):
+        break
+    try:
+        broj=int(unos())
+    except:
+        print("nije unesen broj")
+        continue
+    else:
+        brojevi.append(broj)
+
+print("Korisnik je unio", len(brojevi), "brojeva.")
+print("Srednja vrijednost:", srednja_vrijednost(brojevi))
+print("Min:", min(brojevi))
+print("Max:", max(brojevi))
+brojevi.sort()
+print(brojevi)
+
+
+#1.4.4.
+words=[]
+file=open("song.txt")
+for line in file:
+    current_words=line.split()
+    for word in current_words:
+        word.lower()
+        words.append(word)
+file.close()
+
+dictionary=dict()
+
+for word in words:
+    dictionary[word]=words.count(word)
+
+count=0
+for word in dictionary:
+    if dictionary[word]==1:
+        count+=1
+        print(word)
+
+print("ima",count,"rijeci koje se pojavljuje jednom")
+
+#1.4.5.
+def prosjecan_broj_rijeci(messages):
+    count=0
+    length=0
+    for message in messages:
+        length+=len(message)
+        words=message.split()
+        count+=len(words)
+    return length/count
+
+hams=[]
+spams=[]
+
+file=open("SMSspamCollection.txt")
+for line in file:
+    line=line.rsplit(" ")
+    if line[0]=="ham":
+        hams.append(line[1])
+    else:
+        spams.append(line[1])
+
+print("prosjecan broj rijeci ham:",prosjecan_broj_rijeci(hams))
+print("prosjecan broj rijeci spams",prosjecan_broj_rijeci(spams))
+
+count=0
+for spam in spams:
+    if spam.endswith("!"):
+        count+=1
+    
+    print(count," spam poruka zavrsava s ukslicnikom")
+
+
+#LV2-----------------------------------------------------------------------------------------------------------------------------
+#2.4.1 crtanje slike
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.array([1,2,3,3,1])
+y=np.array([1,2,2,1,1])
+plt.plot(x,y,'b',linewidth=1,marker=".",markersize=10)
+plt.exis([0.0,4.0,0.0,4.0])
+plt.xlabel("x os")
+plt.ylabel("y os")
+plt.title("primjer")
+plt.show
+
+
+#2.4.2 data.csv sadrži mjerenja visine i mase na M i Ž.Učitaj podatke u numpy polje
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=np.genfromtxt("LV2\data.csv",delimiter=",",names=True)
+
+#a Na temelju velicine numpy polja data, na koliko osoba su izvršena mjerenja?
+print("mjerenja izvrsena na ",len(data),"osoba.")
+#b Prikažite odnos visine i mase osobe pomocu naredbe ´ matplotlib.pyplot.scatter
+plt.scatter(data["Height"],data["Weight"],mareker=".",linewidths=0.1)
+plt.xlabel("visina u cm")
+plt.ylabel("masa [kg]")
+plt.title("odnos visine i mase")
+plt.show()
+#c  Ponovite prethodni zadatak, ali prikažite mjerenja za svaku pedesetu osobu na slici
+plt.scatter(data["height"][0::50],data["weight"][0::50],marker=".",linewidths=0.1)
+plt.xlabel("visina u cm")
+plt.ylabel("masas u kg")
+plt.title("odnos visinei mases")
+plt.show()
+#d Izracunajte i ispišite u terminal minimalnu, maksimalnu i srednju vrijednost visine u ovom podatkovnom skupu
+print("minimalna visina",np.min(data["height"]))
+print("max",np.max(data["height"]))
+print("mean",np.mean(data["height"]))
+#e Ponovite zadatak pod d), ali samo za muškarce, odnosno žene. 
+ind_m=(data[:,0]==1)
+ind_z=(data[:,0]==0)
+print("muskarci:")
+print("min",np.min(data["height"],where=ind_m))
+print("max",np.max(data["height"],where=ind_m))
+print("mean",np.mean(data["height"],where=ind_m))
+print("zene")
+print("min",np.min(data["height"],where=ind_z))
+print("max",np.max(data["height"],where=ind_z))
+print("mean",np.mean(data["height"],where=ind_z))
+
+
+#2.4.3 Skripta zadatak_3.py ucitava sliku ’ ˇ road.jpg’.
+import numpy as np
+import matplotlib.pyplot as plt
+
+img=plt.imread("LV2\\road.jpg")
+img=img[:,:,0].copy()
+
+plt.figure()
+
+#a posvijetliti sliku
+plt.imshow(img,cmap="gray",alpha=0.5)
+plt.show()
+#b prikazati samo drugu cetvrtinu slike po širini
+plt.imshow(img[:,160:320:],cmap="gray")
+plt.show()
+#c zarotirati sliku za 90 stupnjeva u smjeru kazaljke na satu
+plt.imshow(np.rot90(img),cmap="gray")
+plt.show()
+#d zrcaliti sliku
+plt.imshow(np.flip(img,axis=1),cmap="gray")
+plt.show()
+
+#2.4.4 Napišite program koji ce kreirati sliku koja sadrži ´ cetiri kvadrata crne odnosno bijele boje 
+import numpy as np
+import matplotlib.pyplot as plt
+
+black=np.zeroes((50,50))
+white=np.ones((50,50))
+
+first=np.hstack([black,white])
+second=np.hstack([black,white])
+
+img=np.vstack([first,second])
+
+plt.figure()
+plt.imshow(img,cmap="gray")
+plt.show()
